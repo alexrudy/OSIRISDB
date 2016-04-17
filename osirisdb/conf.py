fc0915b4181b463fc3b35771c972a16d3736656d
Alex Rudy <alex.rudy@gmail.com>
2016-04-17 14:34:08 -0700

    Add optional config for postgres.

--- osirisdb/conf.py
+++ osirisdb/conf.py
@@ -1,3 +1,4 @@
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://osiris:osiris@localhost:5432/osiris'
 SECRET_KEY = 'OSIRIS is the best'
